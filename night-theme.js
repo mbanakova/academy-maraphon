9197523641362c99529fb03ed4711aaba43f57d0
mbanakova <63152399+mbanakova@users.noreply.github.com>
2020-04-28 20:10:11 +0300

    Delete night-theme.js

--- night-theme.js
+++
@@ -1,7 +0,0 @@
-let page = document.querySelector('.page');
-let themeButton = document.querySelector('.theme-button');
-console.log(page);
-themeButton.onclick = function() {
-page.classList.toggle('light-theme');
-page.classList.toggle('dark-theme');
-};
